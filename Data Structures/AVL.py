4a9d6b8bb1f96508b85a84a5a5fae526964f94e8
jfang96 <jfang.96@gmail.com>
2022-01-17 12:05:53 -0800

    Copy AVL and AVL_test from BST and BST_test. Add rotate methods

---
+++ Data Structures/AVL.py
@@ -0,0 +1,181 @@
+class AVLNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.height = 0
+        self.bf = 0 # Balance factor
+    
+    def update(self):
+        ''' Updates the nodes height and balance factor '''
+        if self.left is None:
+            leftHeight = -1
+        else:
+            leftHeight = self.left.height
+        if self.right is None:
+            rightHeight = -1
+        else:
+            rightHeight = self.right.height
+
+        self.height = max(leftHeight, rightHeight) + 1
+        self.bf = leftHeight - rightHeight
+
+    def details(self):
+        ''' Returns node data, height, and balance factor'''
+        self.update()
+        print('Data: {data}, Height: {height}, BF: {bf}'.format(data=self.data, height=self.height, bf=self.bf))
+        return (self.data, self.height, self.bf)
+
+
+
+class AVL:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def display(self, sortType = "inorder"):
+        arr = []
+
+        if sortType == "inorder":
+            self.inorder(self.root, arr)
+        elif sortType == "preorder":
+            self.preorder(self.root, arr)
+        elif sortType == "postorder":
+            self.postorder(self.root, arr)
+        elif sortType == "levelorder":
+            self.levelorder(self.root, arr)
+
+        print(arr)
+        return arr
+
+    def inorder(self, node, arr):
+        if node is None:
+            return
+
+        self.inorder(node.left, arr)
+        arr.append(node.data)
+        self.inorder(node.right, arr)
+
+    def preorder(self, node, arr):
+        if node is None:
+            return
+        
+        arr.append(node.data)
+        self.preorder(node.left, arr)
+        self.preorder(node.right, arr)
+
+    def postorder(self, node, arr):
+        if node is None:
+            return
+        
+        self.postorder(node.left, arr)
+        self.postorder(node.right, arr)
+        arr.append(node.data)
+            
+    def levelorder(self, node, arr):
+        queue = [node]
+        front = 0
+        while front < len(queue):
+            curr = queue[front]
+            front += 1
+            if curr is not None:
+                arr.append(curr.data)
+                queue.append(curr.left)
+                queue.append(curr.right)
+                
+    def contains(self, data):
+        return self.rContains(self.root, data)
+
+    def rContains(self, node, data):
+        if node is None:
+            return False
+        if node.data == data:
+            return (True, node)
+
+        if data > node.data:
+            return self.rContains(node.right, data)
+        elif data < node.data:
+            return self.rContains(node.left, data)
+            
+
+    def add(self, data):
+        '''  Add the data as a leaf in the BST. Should traverse the tree to find
+         the appropriate location. If the data is already in the tree, then
+         nothing should be done (the duplicate shouldn't get added, and size
+         should not be incremented).
+
+         Should have a running time of O(log n) for a balanced tree, and a
+         worstcase of O(n).
+
+         Args:
+         data -- data to add
+         '''
+        print("start")
+        self.root = self.rAdd(self.root, data)
+
+    def rAdd(self, curr, data):
+        ''' Recursive helper method for add '''
+        if curr is None: # Add to empty tree
+            self.size += 1
+            return AVLNode(data)
+        if data < curr.data: # Traverse left
+            curr.left = self.rAdd(curr.left, data)
+        elif data > curr.data: # Traverse right
+            curr.right = self.rAdd(curr.right, data)
+        curr.update()       
+        curr.details() 
+        return curr
+
+    def remove(self, data):
+        self.root = self.rRemove(self.root, data)
+
+    def rRemove(self, curr, data):
+        if curr is None:
+            return None # data not found
+        
+        if data < curr.data: 
+            curr.left = self.rRemove(curr.left, data)
+        elif data > curr.data:
+            curr.right = self.rRemove(curr.right, data)
+        else: # Data found
+            if curr.left is None and curr.right is None: # 0 child case
+                return None
+            if curr.left is not None and curr.right is not None: # Two child case
+                tempNode = AVLNode() # Create dummy node to hold removed Successor
+                curr.right = self.removeSuccessor(curr.right, tempNode) # Set right subtree
+                curr.data = tempNode.data # Update current node's data with the successor data
+            elif curr.left is not None: # 1 child case (left)
+                return curr.left
+            elif curr.right is not None: # 1 child case (right)
+                return curr.right 
+
+    def removeSuccessor(self, curr, tempNode):
+        ''' Remove successor '''
+        if curr.left is None:
+            tempNode.data = curr.data
+            return curr.right
+        else:
+            curr.left = self.removeSuccessor(curr.left, tempNode)
+
+    def removePredecessor(self, curr, tempNode):
+        if curr.right is None:
+            tempNode.data = curr.data
+            return curr.left
+        else:
+            curr.right = self.removePredecessor(curr.right, tempNode)
+
+    def rotateLeft(self, node):
+        pivotNode = node.right
+        node.right = pivotNode.left
+        pivotNode.left = node
+        node.update()
+        pivotNode.update()
+        return pivotNode
+
+    def rotateRight(self, node):
+        pivotNode = node.left
+        node.left = pivotNode.right
+        pivotNode.right = node
+        node.update()
+        pivotNode.update()
+        return pivotNode
---
+++ Data Structures/AVL_test.py
@@ -0,0 +1,44 @@
+# AVL TEST
+import unittest
+from AVL import AVL
+
+class TestBSTMethods(unittest.TestCase):
+    def setUp(self) -> None:
+        self.avl = AVL()
+
+    def test_add(self):
+        arr = [8, 3, 1, 6, 4, 7, 10, 14, 13, 13]
+        for num in arr:
+            self.avl.add(num)
+        
+        self.assertEqual(self.avl.display(), [1, 3, 4, 6, 7, 8, 10, 13, 14])
+
+    def test_remove(self):
+        return
+
+    def test_traversals(self):
+        arr = [4, 2, 6, 1, 3, 5, 7]
+        for num in arr:
+            self.avl.add(num)
+
+        self.assertEqual(self.avl.display("inorder"), [1, 2, 3, 4, 5, 6, 7], "inorder traversal wrong")
+        self.assertEqual(self.avl.display("preorder"), [4, 2, 1, 3, 6, 5, 7], "preorder traversal wrong")
+        self.assertEqual(self.avl.display("postorder"), [1, 3, 2, 5, 7, 6, 4], "postorder traversal wrong")
+        self.assertEqual(self.avl.display("levelorder"), [4, 2, 6, 1, 3, 5, 7], "levelorder wrong")
+        return
+
+    def test_contains(self):
+        arr = [4, 2, 6, 1, 3, 5, 7]
+        for num in arr:
+            self.avl.add(num)
+
+        self.assertTrue(self.avl.contains(4)) # Check root
+        self.assertTrue(self.avl.contains(5)) # Check left leaf
+        self.assertTrue(self.avl.contains(2)) # Check left parent
+        self.assertTrue(self.avl.contains(3)) # Check right leaf
+
+        self.assertFalse(self.avl.contains(8)) 
+        self.assertFalse(self.avl.contains(0))
+
+if __name__ == '__main__':
+    unittest.main()
