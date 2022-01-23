68e7cdba3e3b8f6a07a964fcd69941853129bc62
jfang96 <jfang.96@gmail.com>
2022-01-23 14:45:40 -0800

    Create tree class, AVL and BST inherit

--- Data_Structures/AVL.py
+++ Data_Structures/AVL.py
@@ -1,8 +1,8 @@
-class AVLNode:
+from BST import BSTNode, BST
+
+class AVLNode(BSTNode):
     def __init__(self, data=None):
-        self.data = data
-        self.left = None
-        self.right = None
+        super().__init__(data)
         self.height = 0
         self.bf = 0 # Balance factor
     
@@ -28,60 +28,7 @@ class AVLNode:
 
 
 
-class AVL:
-    def __init__(self):
-        self.root = None
-        self.size = 0
-
-    def display(self, sortType = "inorder"):
-        arr = []
-
-        if sortType == "inorder":
-            self.inorder(self.root, arr)
-        elif sortType == "preorder":
-            self.preorder(self.root, arr)
-        elif sortType == "postorder":
-            self.postorder(self.root, arr)
-        elif sortType == "levelorder":
-            self.levelorder(self.root, arr)
-
-        print(arr)
-        return arr
-
-    def inorder(self, node, arr):
-        if node is None:
-            return
-
-        self.inorder(node.left, arr)
-        arr.append(node.data)
-        self.inorder(node.right, arr)
-
-    def preorder(self, node, arr):
-        if node is None:
-            return
-        
-        arr.append(node.data)
-        self.preorder(node.left, arr)
-        self.preorder(node.right, arr)
-
-    def postorder(self, node, arr):
-        if node is None:
-            return
-        
-        self.postorder(node.left, arr)
-        self.postorder(node.right, arr)
-        arr.append(node.data)
-            
-    def levelorder(self, node, arr):
-        queue = [node]
-        front = 0
-        while front < len(queue):
-            curr = queue[front]
-            front += 1
-            if curr is not None:
-                arr.append(curr.data)
-                queue.append(curr.left)
-                queue.append(curr.right)
+class AVL(BST):
                 
     def contains(self, data):
         return self.rContains(self.root, data)
--- Data_Structures/BST.py
+++ Data_Structures/BST.py
@@ -1,64 +1,10 @@
-class BSTNode:
-    def __init__(self, data=None):
-        self.data = data
-        self.left = None
-        self.right = None
+from Tree import TreeNode, Tree
 
-class BST:
-    def __init__(self):
-        self.root = None
-        self.size = 0
+class BSTNode(TreeNode):
+    pass
 
-    def display(self, sortType = "inorder"):
-        arr = []
-
-        if sortType == "inorder":
-            self.inorder(self.root, arr)
-        elif sortType == "preorder":
-            self.preorder(self.root, arr)
-        elif sortType == "postorder":
-            self.postorder(self.root, arr)
-        elif sortType == "levelorder":
-            self.levelorder(self.root, arr)
-
-        print(arr)
-        return arr
-
-    def inorder(self, node, arr):
-        if node is None:
-            return
-
-        self.inorder(node.left, arr)
-        arr.append(node.data)
-        self.inorder(node.right, arr)
-
-    def preorder(self, node, arr):
-        if node is None:
-            return
-        
-        arr.append(node.data)
-        self.preorder(node.left, arr)
-        self.preorder(node.right, arr)
-
-    def postorder(self, node, arr):
-        if node is None:
-            return
-        
-        self.postorder(node.left, arr)
-        self.postorder(node.right, arr)
-        arr.append(node.data)
-            
-    def levelorder(self, node, arr):
-        queue = [node]
-        front = 0
-        while front < len(queue):
-            curr = queue[front]
-            front += 1
-            if curr is not None:
-                arr.append(curr.data)
-                queue.append(curr.left)
-                queue.append(curr.right)
-                
+class BST(Tree):
+    ''' Inherits properties of Tree class '''
     def contains(self, data):
         return self.rContains(self.root, data)
 
