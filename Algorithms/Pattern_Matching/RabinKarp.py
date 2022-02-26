d3170a7cb5ef1957e4d500681a95eebc95225746
jfang96 <jfang.96@gmail.com>
2022-02-26 14:38:20 -0800

    Implement Rabin-Karp pattern matchihng algorithm

--- Algorithms/Pattern_Matching/RabinKarp.py
+++ Algorithms/Pattern_Matching/RabinKarp.py
@@ -1,15 +1,33 @@
 def RabinKarp(pattern, text):
+    ''' Rabin-Karp Pattern-matching algorithm 
+        Time: O(mn)
+        Space: O(1); pattern and text hashes, results arr    
+    '''
+    res = []
+
     m = len(pattern)
     n = len(text)
     base = 2
 
-    hash = calcHash(pattern, base) # Calculate initial hash
-    print(hash)
+    hash = calcHash(pattern, base) # Calculate initial pattern hash
+    textHash = calcHash(text[:m], base) # Calculate hash of first length-m substring in text
 
-    oldChar = "L"
-    newChar = "Q"
-    hash = updateHash(hash, oldChar, newChar, base)
-    print(hash)
+    i = 0
+    while i <= n - m: # Iterate through text
+        if hash == textHash: # Hashes match! Now compare char by char
+            iText = i
+            iPattern = 0
+            while text[iText] == pattern[iPattern]: # Compare char by char
+                if iPattern == m - 1: # Complete match found
+                    res.append(i) 
+                    break
+                iText += 1
+                iPattern += 1
+        if i < m - n: # Check if at end of text
+            textHash = updateHash(textHash, text[i], text[i+m], base) # Update text hash with next char
+        i += 1 
+
+    return res
 
 def calcHash(pattern, base):
     ''' Calculate hash of pattern with base. Assume hash function is ASCII. '''
@@ -26,6 +44,6 @@ def updateHash(oldHash, oldChar, newChar, base):
     
 
 # Testing
-pattern = "Linked"
-text = ""
+pattern = "aaab"
+text = "aaabaaab"
 print(RabinKarp(pattern, text))
