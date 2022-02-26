b47b0d159e72b27c0d42c5c0de7fb119e7148906
jfang96 <jfang.96@gmail.com>
2022-02-26 13:11:33 -0800

    Implement KMP algorithm and add comments to failureTable function

--- Algorithms/Pattern_Matching/KMP.py
+++ Algorithms/Pattern_Matching/KMP.py
@@ -1,18 +1,22 @@
 from lib2to3.pygram import pattern_symbols
 
 
+def KMP(pattern, text):
+    ft = failureTable(pattern)
+
 def failureTable(pattern):
     ft = [0] * len(pattern)
-    i, j = 0
-    while j < len(pattern):
-        if pattern[i] == pattern[j]:
-            ft[j] = i + 1
-            i += 1
-            j += 1
-        elif pattern[i] != pattern[j] and i == 0:
-            ft[j] = 0
-            j += 1
+    prefix, query = 0 # Create indices
+    while query < len(pattern):
+        # If match is found, 
+        if pattern[prefix] == pattern[query]:
+            ft[query] = prefix + 1
+            prefix += 1
+            query += 1
+        elif pattern[prefix] != pattern[query] and prefix == 0:
+            ft[query] = 0
+            query += 1
         else:
-            i = ft[i-1]
+            prefix = ft[prefix-1]
     
     return ft
