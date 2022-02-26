9ae9b5923196a27523f8aaac2d5b70b00ea8a5d1
jfang96 <jfang.96@gmail.com>
2022-02-26 14:16:33 -0800

    Implement updateHash function for Rabin-Karp

--- Algorithms/Pattern_Matching/RabinKarp.py
+++ Algorithms/Pattern_Matching/RabinKarp.py
@@ -1,8 +1,14 @@
 def RabinKarp(pattern, text):
     m = len(pattern)
     n = len(text)
+    base = 2
 
-    hash = calcHash(pattern, 2) # Calculate initial hash with base = 2
+    hash = calcHash(pattern, base) # Calculate initial hash
+    print(hash)
+
+    oldChar = "L"
+    newChar = "Q"
+    hash = updateHash(hash, oldChar, newChar, base)
     print(hash)
 
 def calcHash(pattern, base):
@@ -13,8 +19,13 @@ def calcHash(pattern, base):
         hash += ord(char) * base**(m-(i+1))
     return hash
 
+def updateHash(oldHash, oldChar, newChar, base):
+    ''' Update current hash by removing oldChar from hash and adding newChar. '''
+    m = len(pattern)
+    return (oldHash - ord(oldChar) * base**(m-1)) * base + ord(newChar)
+    
 
 # Testing
-pattern = "KnuthM"
+pattern = "Linked"
 text = ""
 print(RabinKarp(pattern, text))
