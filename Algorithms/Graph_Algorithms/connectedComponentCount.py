d42ab791eff8e188514d1e26b322781b2a3362bc
jfang96 <jfang.96@gmail.com>
2022-07-17 12:11:50 -0700

    Implement connectedComponentCount

---
+++ Algorithms/Graph_Algorithms/connectedComponentCount.py
@@ -0,0 +1,29 @@
+def connectedComponentCount(graph):
+    count = [0]
+    visited = set()
+
+    def dfs(graph, node):
+        if node in visited:
+            return
+        visited.add(node)
+        count[0] += 1
+        for neighbor in graph[node]:
+            dfs(graph, neighbor)
+
+    for node in graph:
+        dfs(graph, node)
+
+    return count[0]
+
+
+graph = {
+    'i': ['j', 'k'], 
+    'j': ['i'], 
+    'k': ['i', 'm', 'l'], 
+    'm': ['k'], 
+    'l': ['k'], 
+    'o': ['n'], 
+    'n': ['o']
+    }
+
+print(connectedComponentCount(graph))
